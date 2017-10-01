81c1cb7bdbc02de408eb9f939be3e40f087ff63e
Patrick Totzke <patricktotzke@gmail.com>
2017-10-01 15:04:23 +0100

    tests: clean up 'test_prefer_html'
    I don't understand the comment about non-determinism and think it's just
    confusing here.

--- tests/db/utils_test.py
+++ tests/db/utils_test.py
@@ -723,8 +723,6 @@ class TestExtractBody(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     # Mock the handler to cat, so that no transformations of the html are made
-    # making the result non-deterministic
-    @mock.patch('alot.db.utils.settings.get', mock.Mock(return_value=False))
     @mock.patch('alot.db.utils.settings.mailcap_find_match',
                 mock.Mock(return_value=(None, {'view': 'cat'})))
     def test_prefer_html(self):
